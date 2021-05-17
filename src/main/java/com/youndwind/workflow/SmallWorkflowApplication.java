05818b555c6c9ca13633b08a4a7d25461c64111f
young-wind <386046628@qq.com>
2021-05-17 18:32:50 +0800

    搭建gradle的springboot项目结构

---
+++ src/main/java/com/youndwind/workflow/SmallWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.youndwind.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallWorkflowApplication.class, args);
+    }
+
+}
